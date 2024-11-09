52cd3dd1d57e5f9ad892da4f8c394a21347d757f
Iskha-69 <iskhak.koichubaev@alatoo.edu.kg>
2024-11-09 21:13:10 +0600

    first commit

---
+++ src/test/java/alatoo/car_rent/CarRentApplicationTests.java
@@ -0,0 +1,13 @@
+package alatoo.car_rent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
